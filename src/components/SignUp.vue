9961aebc9513b8c5643af31ee9c405edd01d3c88
Yurii Rybachok <drummer2009@ukr.net>
2023-06-08 22:14:28 +0300

    create SignUp component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <img class="logo" src="../assets/resto-logo.jpg" alt="Logo" />
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name" />
+        <input type="text" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter Password" />
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.logo {
+    width: 100px;
+}
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
