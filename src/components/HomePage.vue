5403f9fa9a41552fc6ba7bffde7918218dd57e23
Yurii Rybachok <drummer2009@ukr.net>
2023-06-09 14:27:26 +0300

    create set up routing & home component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Wellcome to Home page</h1>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -32,8 +32,8 @@ export default {
             console.warn(result);
 
             if (result.status == 201) {
-                alert("sign-up done")
                 localStorage.setItem("user-info", JSON.stringify(result.data))
+                this.$router.push({name: 'HomePage'})
             }
         }
     },
