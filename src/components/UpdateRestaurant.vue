8df478c65819003d0a63334066511a4f0f2a1b4b
Yurii Rybachok <drummer2009@ukr.net>
2023-06-10 12:04:39 +0300

    update restaurant form

--- src/components/UpdateRestaurant.vue
+++ src/components/UpdateRestaurant.vue
@@ -6,7 +6,7 @@
         <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name" />
         <input type="text" name="address" placeholder="Enter Address" v-model="restaurant.address" />
         <input type="text" name="contact" placeholder="Enter Contact" v-model="restaurant.contact" />
-        <button type="button" @click="addRestaurant">Update Restaurant</button>
+        <button type="button" @click="updateRestaurant">Update Restaurant</button>
     </form>
 </template>
 
@@ -27,6 +27,19 @@ export default {
                 name: '',
                 address: '',
                 contact: ''
+            }
+        }
+    },
+
+    methods: {
+        async updateRestaurant() {
+            const result = await axios.put("http://localhost:3000/restaurant/" + this.$route.params.id, {
+                name: this.restaurant.name,
+                address: this.restaurant.address,
+                contact: this.restaurant.contact,
+            });
+            if (result.status == 200) {
+                this.$router.push({ name: 'HomePage' })
             }
         }
     },
