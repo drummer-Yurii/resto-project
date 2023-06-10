dc6a1d317209871498ac4241b0c20d872dc3b70b
Yurii Rybachok <drummer2009@ukr.net>
2023-06-10 11:23:37 +0300

    update restaurant & route

--- src/components/UpdateRestaurant.vue
+++ src/components/UpdateRestaurant.vue
@@ -1,6 +1,13 @@
 <template>
     <HeaderPage />
-    <h1>Wellcome to UpdateRestaurant page</h1>
+    <h1>Hello User, Wellcome to UpdateRestaurant page</h1>
+
+    <form class="add">
+        <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name" />
+        <input type="text" name="address" placeholder="Enter Address" v-model="restaurant.address" />
+        <input type="text" name="contact" placeholder="Enter Contact" v-model="restaurant.contact" />
+        <button type="button" @click="addRestaurant">Update Restaurant</button>
+    </form>
 </template>
 
 <script>
@@ -10,6 +17,16 @@ export default {
 
     components: {
         HeaderPage
+    },
+
+    data() {
+        return {
+            restaurant: {
+                name: '',
+                address: '',
+                contact: ''
+            }
+        }
     },
 
     mounted() {
